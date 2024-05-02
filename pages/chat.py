d82c06186aa55636961cdb6cabe4da48044cf506
doodoo77 <cdscdscdsqq@gmail.con>
2024-05-02 20:38:18 +0900

    Langchain (Not completed yet)

--- pages/chat.py
+++ pages/chat.py
@@ -2,8 +2,11 @@ import streamlit as st
 # from openai import OpenAI
 
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories.streamlit import StreamlitChatMessageHistory
 
+from prompts import pos_prompt, neg_prompt
 
 # client = OpenAI(
 #     api_key=st.secrets['OPENAI_API_KEY'], 
@@ -37,34 +40,56 @@ st.markdown("---")
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
+if "chain" not in st.session_state:
+    st.session_state["chain"] = "positive"
+    
 # Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = [
-        {"role": "system", "content": "Your helpful assistant."},
-        {"role": "assistant", "content": "대화 시작 시 인사말 여기에"}
-    ]
+# if "messages" not in st.session_state:
+#     st.session_state.messages = [
+#         {"role": "system", "content": "Your helpful assistant."},
+#         {"role": "assistant", "content": "대화 시작 시 인사말 여기에"}
+#     ]
+
+# Set up memory
+msgs = StreamlitChatMessageHistory(key="langchain_messages")
+if len(msgs.messages) == 0:
+    msgs.add_ai_message("How can I help you?")
 
 # Display chat messages from history on app rerun
-for message in st.session_state.messages[1:]:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+# for message in st.session_state.messages:
+#     if message["role"] == "system":
+#         continue
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
+
+for msg in msgs.messages:
+    st.chat_message(msg.type).write(msg.content)
 
 # ----------------------------------------------------------------------------------------------------
 # Chains
 # ----------------------------------------------------------------------------------------------------
 pos_prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are helpful assistant."),
+    ("system", pos_prompt),
+    MessagesPlaceholder(variable_name="history"),
     ("user", "{input}")
 ])
 
 pos_chain = pos_prompt | fast_llm
 
+pos_chain_with_history = RunnableWithMessageHistory(
+    pos_chain,
+    lambda session_id: msgs,
+    input_messages_key="input",
+    history_messages_key="history",
+)
+
 neg_prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are helpful assistant."),
+    ("system", neg_prompt),
+    MessagesPlaceholder(variable_name="history"),
     ("user", "{input}")
 ])
 
-neg_chain = pos_prompt | fast_llm
+neg_chain = neg_prompt | fast_llm
 
 # ----------------------------------------------------------------------------------------------------
 # Chat
@@ -73,7 +98,7 @@ neg_chain = pos_prompt | fast_llm
 if user_input := st.chat_input("여기에 입력 관련 설명 가능"):
     
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": user_input})
+    # st.session_state.messages.append({"role": "user", "content": user_input})
     
     # Display user message in chat message container
     with st.chat_message("user"):
@@ -89,10 +114,21 @@ if user_input := st.chat_input("여기에 입력 관련 설명 가능"):
         #     ],
         #     stream=True,
         # )
-        stream = pos_chain.stream({"input": user_input})
-        response = st.write_stream(stream)
+        # st.write(st.session_state.chain)
+        # if st.session_state.chain == "positive":
+        #     stream = pos_chain.stream({"input": user_input})
+        # elif st.session_state.chain == "negative":
+        #     stream = neg_chain.stream({"input": user_input})
+        # response = st.write_stream(stream)
+        config = {"configurable": {"session_id": "any"}}
+        response = pos_chain_with_history.invoke({"input": user_input}, config)
+        st.markdown(response.content)            
+        
+    # Check
+    if "you will debate with an opponent chatbot" in response:
+        st.session_state.chain = "negative"
         
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    # st.session_state.messages.append({"role": "assistant", "content": response})
         
         
